49b846311239d02ad4c0ab62bf69a9505256b710
liuwin7 <liuwin7@163.com>
2021-09-26 17:44:30 +0800

    调整配置

--- openvidu-call-back/src/config.ts
+++ openvidu-call-back/src/config.ts
@@ -1,5 +1,5 @@
-export const SERVER_PORT = process.env.SERVER_PORT || 5000;
+export const SERVER_PORT = process.env.SERVER_PORT || 40002;
 // export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://192.168.8.181:4443';
-export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://boyfu.xyz';
+export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://boyfu.xyz:40001';
 export const OPENVIDU_SECRET = process.env.OPENVIDU_SECRET || 'MY_SECRET';
 export const CALL_OPENVIDU_CERTTYPE = process.env.CALL_OPENVIDU_CERTTYPE;
