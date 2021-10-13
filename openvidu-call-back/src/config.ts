5cdcc4887f34f3cb61224b28efd799f7e9934a45
liuwin7 <liuwin7@163.com>
2021-10-13 17:24:41 +0800

    修改配置

--- openvidu-call-back/src/config.ts
+++ openvidu-call-back/src/config.ts
@@ -1,5 +1,9 @@
 export const SERVER_PORT = process.env.SERVER_PORT || 40002;
-// export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://192.168.8.181:4443';
-export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://uniplay.boyfu.xyz:40001';
+// TODO: - 由于公司网络不允许内网的请求，所以，使用内网的HTTPS连接，并修改CertType，并忽略HTTPS的验证
+export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://172.16.0.127:40002';
+export const CALL_OPENVIDU_CERTTYPE = process.env.CALL_OPENVIDU_CERTTYPE || 'selfsigned';
+
+// export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://uniplay.boyfu.xyz:40001';
+// export const CALL_OPENVIDU_CERTTYPE = process.env.CALL_OPENVIDU_CERTTYPE || 'owncert';
+
 export const OPENVIDU_SECRET = process.env.OPENVIDU_SECRET || 'openvidu';
-export const CALL_OPENVIDU_CERTTYPE = process.env.CALL_OPENVIDU_CERTTYPE;
