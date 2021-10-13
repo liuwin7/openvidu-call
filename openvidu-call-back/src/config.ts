881ed62b4c22f1395ef7de6bba3034939c821b33
liuwin7 <liuwin7@163.com>
2021-10-13 16:45:34 +0800

    修改配置

--- openvidu-call-back/src/config.ts
+++ openvidu-call-back/src/config.ts
@@ -1,5 +1,5 @@
 export const SERVER_PORT = process.env.SERVER_PORT || 40002;
 // export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://192.168.8.181:4443';
-export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://boyfu.xyz:40001';
+export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://uniplay.boyfu.xyz:40001';
 export const OPENVIDU_SECRET = process.env.OPENVIDU_SECRET || 'openvidu';
 export const CALL_OPENVIDU_CERTTYPE = process.env.CALL_OPENVIDU_CERTTYPE;
