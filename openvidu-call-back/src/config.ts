1d79577729c5df802eb7db0eeaa515d118762a98
liuwin7 <liuwin7@163.com>
2022-06-14 14:38:45 +0800

    增加OPTIONS

--- openvidu-call-back/src/config.ts
+++ openvidu-call-back/src/config.ts
@@ -1,4 +1,4 @@
-export const SERVER_PORT = process.env.SERVER_PORT || 41002;
+export const SERVER_PORT = process.env.SERVER_PORT || 40002;
 // TODO: - 由于公司网络不允许内网的请求，所以，使用内网的HTTPS连接，并忽略HTTPS的验证
 export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://10.235.66.18:40001';
 // export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://uniplay.boyfu.xyz:40001';
