cb9318a66b069dda5f9723127ec1c1bb455f2084
liuwin7 <liuwin7@163.com>
2023-01-11 16:17:44 +0800

    调整配置文件

--- openvidu-call-back/src/config.ts
+++ openvidu-call-back/src/config.ts
@@ -1,5 +1,10 @@
 export const SERVER_PORT = process.env.SERVER_PORT || 40002;
 // TODO: - 由于公司网络不允许内网的请求，所以，使用内网的HTTPS连接，并忽略HTTPS的验证
-export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://10.235.66.18:40001';
+
+// export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'http://localhost:5443';
+//export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'http://120.48.83.53:5443'
+//export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'http://raymedy.cn:5443'
+//export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'https://uniplay.cm-topsci.com:40001';
+export const OPENVIDU_URL = process.env.OPENVIDU_URL || 'http://61.51.178.38:40043'
 export const CALL_OPENVIDU_CERTTYPE = process.env.CALL_OPENVIDU_CERTTYPE || 'owncert';
 export const OPENVIDU_SECRET = process.env.OPENVIDU_SECRET || 'openvidu';
