aa110884d7b6c951110278e1de9fcb36dff53c89
liuwin7 <liuwin7@163.com>
2023-01-11 17:59:02 +0800

    增加https

--- openvidu-call-back/src/app.ts
+++ openvidu-call-back/src/app.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
-import { SERVER_PORT, OPENVIDU_URL, OPENVIDU_SECRET, CALL_OPENVIDU_CERTTYPE } from './config';
+import { SERVER_PORT, OPENVIDU_URL, OPENVIDU_SECRET, CALL_OPENVIDU_CERTTYPE, USE_SSL } from './config';
 import { app as callController } from './controllers/CallController';
 import { app as dashboardController } from './controllers/DashboardController';
 import * as dotenv from 'dotenv';
 import * as http from 'http';
+import * as https from 'https';
+import * as fs from "fs";
 import * as _ from 'lodash';
 import * as ExpressWs from 'express-ws';
 import { WebSocket } from 'ws';
@@ -21,20 +23,29 @@ export const wsDB: {
 
 const app = express();
 
-const httpServer = http.createServer(app)
-    .on('listening', () => {
-        console.log("---------------------------------------------------------");
-        console.log(" ")
-        console.log(`OPENVIDU URL: ${OPENVIDU_URL}`);
-        console.log(`OPENVIDU SECRET: ${OPENVIDU_SECRET}`);
-        console.log(`CALL OPENVIDU CERTTYPE: ${CALL_OPENVIDU_CERTTYPE}`);
-        console.log(`OpenVidu Call Server is listening on port ${SERVER_PORT}`);
-        console.log(" ")
-        console.log("---------------------------------------------------------");
-    })
-    .listen(SERVER_PORT);
+const listeningHandler = () => {
+    console.log("---------------------------------------------------------");
+    console.log(" ")
+    console.log(`OPENVIDU URL: ${OPENVIDU_URL}`);
+    console.log(`OPENVIDU SECRET: ${OPENVIDU_SECRET}`);
+    console.log(`CALL OPENVIDU CERTTYPE: ${CALL_OPENVIDU_CERTTYPE}`);
+    console.log(`OpenVidu Call Server is listening on port ${SERVER_PORT}`);
+    console.log(`Openvidu Call Server uses ${USE_SSL ? "HTTPS" : "HTTP"}`);
+    console.log(" ")
+    console.log("---------------------------------------------------------");
+};
 
-const { app: wsApp } = ExpressWs(app, httpServer);
+const server = USE_SSL
+    ? https.createServer({
+        key: fs.readFileSync(__dirname + '/certs/private.key'),
+        cert: fs.readFileSync(__dirname + '/certs/full_chain.pem'),
+    }, app)
+    : http.createServer(app);
+    
+// start server listening
+server.on("listening", listeningHandler).listen(SERVER_PORT);
+
+const { app: wsApp } = ExpressWs(app, server);
 
 // 需要再express-ws初始化以后再导入，否则会报错，参考
 // https://github.com/HenningM/express-ws#:~:text=before%20loading%20or%20defining%20your%20routers
