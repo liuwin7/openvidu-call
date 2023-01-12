964dec21ad2ce12e5574325cfd8d0d0e1f5eb36a
liuwin7 <liuwin7@163.com>
2023-01-12 18:02:06 +0800

    移除调试信息

--- openvidu-call-back/src/app.ts
+++ openvidu-call-back/src/app.ts
@@ -3,9 +3,6 @@ import * as dotenv from 'dotenv';
 // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 
-console.log(process.env);
-
-
 import {
     SERVER_PORT, OPENVIDU_URL, OPENVIDU_SECRET,
     CALL_OPENVIDU_CERTTYPE, SERVER_TYPE
